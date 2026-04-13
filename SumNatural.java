f9493ee3689dc1c6514a6e64d49f7f842d56e458
Manat Rajput <manatrajput25165@gmail.com>
2026-04-13 19:12:31 +0530

    SumNatural.java

---
+++ SumNatural.java
@@ -0,0 +1,14 @@
+public class SumNatural {
+ public static void main(String[] args)
+    {
+        int N = 10;
+        int sum = 0;
+        System.out.println("Finding Sum of " + N );
+
+        for (int i = 1; i <= N; i++) {
+            sum += i;
+        }
+      
+        System.out.println("Sum of first " + N + " Natural Number = " + sum);
+    }
+}
