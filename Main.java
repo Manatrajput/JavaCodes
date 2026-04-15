27c2b9669188fe74ea36750255a2b6c45800b6e5
Manat Rajput <manatrajput25165@gmail.com>
2026-04-15 21:38:27 +0530

    Main.java

---
+++ Main.java
@@ -0,0 +1,27 @@
+class Demo {
+
+    int a;
+    String name;
+
+    Demo() {
+        a = 0;
+        name = null;
+    }
+
+
+    void display() {
+        System.out.println("Value of a: " + a);
+        System.out.println("Name: " + name);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        Demo obj = new Demo();
+
+        obj.display();
+
+    }
+}
