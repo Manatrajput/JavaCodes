7ebe503b1d2ea4d1adf20f54a26d91139226a11d
Manat Rajput <manatrajput25165@gmail.com>
2026-04-21 19:51:42 +0530

    locker.java

---
+++ Mannat.java
@@ -0,0 +1,39 @@
+class locker{
+    private int pin =  1234;
+    private boolean isOpen = false;
+    public void openlocker(int enteredpin) {
+
+        if (enteredpin == pin) {
+            isOpen = true;
+            System.out.println("locker opened");
+        } else {
+            System.out.println("wrong pin");
+        }
+    }
+    
+    public void checkstatus() {
+        if (isOpen) {
+            System.out.println("locker is open");
+        } else {
+            System.out.println("locker is closed");
+        }
+    }
+}
+public class Mannat{
+    
+    public static void main(String[] args) {
+{
+    locker A = new locker();
+    A.checkstatus();
+    A.openlocker(1111);
+    A.openlocker(1234);
+    A.checkstatus();
+ }
+
+}
+}
+
+
+
+    
+git
