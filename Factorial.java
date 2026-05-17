ff803507928c4a4f66d88476666b94a4886d2830
Manatrajput <manatrajput25165@gmail.com>
2026-05-17 20:01:27 +0530

    Add files via upload

--- Factorial.java
+++ Factorial.java
@@ -1,18 +1,25 @@
 import java.util.Scanner;
- public class Factorial { 
-    public static void main(String[] args) {
-        int factorial = 1;
-    Scanner sc = new Scanner(System.in);
-    System.out.println("enter a number");
-
- System.out.println("n");
-         int n = sc.nextInt();
-        for (int i=1;i<=n;i++) {
-factorial = factorial * i;
-
-    
+/**
+ * 12. Find factorial for given no Program in Java
+ */
+public class Factorial {
+	public static void main(String[] args) {
+	Scanner scan = new Scanner(System.in);
+		try {
+			System.out.println("Enter the Factorail Number: ");
+			int num = scan.nextInt();
+			if (num>=1) {
+				int temp = 1;
+				for (int i = num; i >= 1; i--) {
+					temp = temp * i;
+				}
+				System.out.println("Factorial Number of "+ num +" is "+ temp);
+			}else {
+				System.out.println("The factorial of a number starts with an integer greater than or equal to one.");
+			}
+			
+		} catch (Exception e) {
+			System.out.println("Invalid Input !!!");	
+			}
+	}
 }
- System.out.println(factorial);
-    }
-
- }
--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,10 @@
- class HelloWorld{
-     public static void main(String[] args) {
-        System.out.println("hello world");
-     }
-     
-      
-     }
+/**
+ * 1.Simple Java Program
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World !!!");
+	}
+
+}
